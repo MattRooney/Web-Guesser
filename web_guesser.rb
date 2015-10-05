8cdd872f34ca916470aa9bbb77600ea396204f83
Matt Rooney <matthewjrooney@gmail.com>
2015-10-05 17:00:59 -0600

    I3: Too high, too low, way too high, way too low and correct messages working properly.

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,7 +1,24 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-number = rand(100)
+Secret_Number = rand(100)
+
 get '/' do
-  erb :index, :locals => {:number => number}
+  guess = params["guess"].to_i
+  message = compare_guess(guess)
+  erb :index, :locals => {:number => Secret_Number, :message => message}
+end
+
+def compare_guess(guess)
+  if guess >= Secret_Number + 5
+    message = "Way too high!"
+  elsif guess > Secret_Number
+    message = "Too high!"
+  elsif guess <= Secret_Number - 5
+    message = "Way too low!"
+  elsif guess < Secret_Number
+    message = "Too low!"
+  elsif guess == Secret_Number
+    message = "You got it!"
+  end
 end
